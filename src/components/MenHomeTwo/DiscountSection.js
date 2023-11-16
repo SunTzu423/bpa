f1447a2b4941d8ca43bc390dc0d535e4e52bf7a5
SunTzu423 <boiboi694201738@gmail.com>
2023-11-16 12:38:30 -0500

    Urethra

--- src/components/MenHomeTwo/DiscountSection.js
+++ src/components/MenHomeTwo/DiscountSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import DishImg from '../../assets/images/gallery_06.png'
+import DishImg from '../../assets/images/gallery_07.png'
 
 function DiscountSection() {
     return (
